418019678810117da1fdd35d3041b5492ecd2f06
Damla Koksal <me@damlakoksal.com>
2021-12-19 19:59:51 +0300

    [#3]test: add test for Main component

--- src/__test__/components/Layout/Layout.test.tsx
+++ src/__test__/components/Layout/Layout.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
-import { FilterList, Layout, Main, Search, Sidebar } from '../../../components';
+import { Layout } from '../../../components';
 
-describe('Tests of FilterList component', () => {
+describe('Tests of Layout component', () => {
   it('Tests of Layout component', () => {
     const { getByTestId } = render(<Layout>test</Layout>);
     expect(getByTestId('layout')).toBeInTheDocument();
